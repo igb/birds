1aed8d26db4bc80112a3236c8792346e9e8b3604
ibrown <ibrown@twitter.com>
2015-05-22 03:39:52 -0700

    extracting single url

--- get-image-for-term.sh
+++ get-image-for-term.sh
@@ -1,1 +1,3 @@
-curl "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=$0" | json_pp | grep unescapedUrl | head -1
+curl "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=$0" | json_pp | grep unescapedUrl | head -1 | sed -E 's/.{30}//' | sed -E 's/.{2}$//' 
+
+
