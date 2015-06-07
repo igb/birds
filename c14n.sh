885b260d1bde914b1d74d28895b7ff1e2613b036
ibrown <ibrown@twitter.com>
2015-06-07 07:49:18 -0700

    added failed lookups with manual corrections/resolutions

--- c14n.sh
+++ c14n.sh
@@ -9,4 +9,42 @@ sed 's/Larus brunicephalus/Chroicocephalus brunnicephalus/g' |
 sed 's/Babulcus ibis/Bubulcus ibis/g' | 
 sed 's/Hirundo fluvicola/Petrochelidon fluvicola/g' | 
 sed 's/Turdoides caudatus/Turdoides caudata/g' | 
-sed 's/Cuculus various/Cuculus varius/g'
+sed 's/Cuculus various/Cuculus varius/g' |
+sed 's/Ploceus benghalensis/Ploceus benghalensis/g' |
+sed 's/Motacilla flava beema/Motacilla flava beema/g' |
+sed 's/Erithacus svecicus/Erithacus svecicus/g' |
+sed 's/Sturnus vulgaris porphyronotus/Sturnus vulgaris porphyronotus/g' |
+sed 's/Tringa hypoleuca/Actitis hypoleucos/g' |
+sed 's/Centropus Sinensis Sinesis/Centropus Sinensis Sinensis/g' |
+sed 's/Pernis Ptilorhychus ruficollis/Pernis Ptilorhynchus ruficollis/g' |
+sed 's/Numenius arguata/Numenius arquata/g' |
+sed 's/Falco peregrinus japonensis/Falco peregrinus japonensis/g' |
+sed 's/Sturnus vulgaris plotaratskyi/Sturnus vulgaris poltoratskiy/g' |
+sed 's/Phoenicopterus reseus/Phoenicopterus roseus/g' |
+sed 's/Motacilla caspica/Motacilla caspica/g' |
+sed 's/Cuculus fugax/Cuculus fugax/g' |
+sed 's/Ptecocles orinetalis/Pterocles orientalis/g' |
+sed 's/Acrocephalus stenotoreus/Acrocephalus stentoreus/g' |
+sed 's/Motacilla alba dukhunensis/Motacilla alba dukhunensis/g' |
+sed 's/Passer Pyrrhonotus/Passer pyrrhonotus/g' |
+sed 's/Phalacrocorax crabo/Phalacrocorax carbo/g' |
+sed 's/Cuculus sparveroides/Hierococcyx sparverioides/g' |
+sed 's/Prodiceps ruficolliis/Podiceps ruficollis/g' |
+sed 's/Beteo rufinus/Buteo rufinus/g' |
+sed 's/Estrilda amandava/Amandava amandava/g' |
+sed 's/Muscicapa parava/Muscicapa parva/g' |
+sed 's/Pycnonotus jacosus/Pycnonotus jocosus/g' |
+sed 's/Streptopelia decapcto/Streptopelia decaocto/g' |
+sed 's/Chaimarronis leucocephalus/Chaimarrornis leucocephalus/g' |
+sed 's/Lanius schack/Lanius schach/g' |
+sed 's/Lochura punctulata/Lonchura punctulata/g' |
+sed 's/Turdoides earlei/Turdoides earlei/g' |
+sed 's/Dendrccitta vagabunda/Dendrocitta vagabunda/g' |
+sed 's/Motacilla flava melanogrisea/Motacilla flava melanogrisea/g' |
+sed 's/Myiophonus caeruleus/Myophonus caeruleus/g' |
+sed 's/Lophura leucomelana hamiltonil/Lophura leucomelanos hamiltoni/g' |
+sed 's/Threskiornis aethopica/Threskiornis aethiopica/g' |
+sed 's/Pericrocotus erythropygius/Pericrocotus erythropygius/g' |
+sed 's/Lochura malabarica/Lonchura malabarica/g' |
+sed 's/Petronia xantocollis/Petronia xanthocollis/g' |
+sed 's/Picoides mahrattensis/Picoides mahrattensis/g' 
