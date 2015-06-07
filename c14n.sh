d2c2d94b537c1d84fd9585fe7f59409ded2b5898
ibrown <ibrown@twitter.com>
2015-06-07 07:21:34 -0700

    more substitutions

--- c14n.sh
+++ c14n.sh
@@ -1,1 +1,12 @@
-sed 's/Tockus birostris/Ocyceros birostris/g' | sed 's/Taccocua leschenaultii/Phaenicophaeus leschenaultii/g' 
+sed 's/Tockus birostris/Ocyceros birostris/g'| 
+sed 's/Taccocua leschenaultii/Phaenicophaeus leschenaultii/g' | 
+sed 's/Francolinus francotinus/Francolinus francolinus/g' | 
+sed 's/Clumba livia/Columba livia/g' | 
+sed 's/Hiliastur indus/Haliastur indus/g' | 
+sed 's/Crovus corax/Corvus corax/g' | 
+sed 's/Amauronis phoenicurus/Amaurornis phoenicurus/g' | 
+sed 's/Larus brunicephalus/Chroicocephalus brunnicephalus/g' | 
+sed 's/Babulcus ibis/Bubulcus ibis/g' | 
+sed 's/Hirundo fluvicola/Petrochelidon fluvicola/g' | 
+sed 's/Turdoides caudatus/Turdoides caudata/g' | 
+sed 's/Cuculus various/Cuculus varius/g'
